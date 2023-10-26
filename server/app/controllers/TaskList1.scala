852ae0f4d2f2dd95919ff30452567a9011fc581a
Jenny Morton <jmorton1@trinity.edu>
2023-10-25 21:19:40 -0500

    vids 14-16 (error with val loginForm, ask Lewis/classmates)

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -4,13 +4,27 @@ import javax.inject._
 import models.TaskListInMemoryModel
 import shared.SharedMessages
 import play.api.mvc._
+//import play.api.i18n._
+import play.api.data._
+import play.api.data.Form._
+
+case class LoginData(username:String,password:String)
 
 @Singleton 
 class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc){
+  val loginForm = Form(mapping(
+    "Username"->text(3,10),
+    "Password"->text(8)
+  )(LoginData.apply)(LoginData.unapply))
+
   def logIn = Action {implicit request =>
     Ok(views.html.login1())
   } //first item occurs on the page first (maybe?)
   
+  def validateLoginForm = Action{ implicit request =>
+    Ok("temp")
+  }
+
   def validateLoginGet(username:String, password:String) = Action {
     Ok(s"$username logged in with $password.")
   }
@@ -46,5 +60,30 @@ class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(c
 
   def logout = Action{
     Redirect(routes.TaskList1.logIn).withNewSession
+  }
+
+  def addTask = Action{implicit request =>
+    val usernameOpt = request.session.get("username")
+    usernameOpt.map{ username =>
+      val postVals = request.body.asFormUrlEncoded    //the "argument" names in login1.scala.html are the keys in this map
+      postVals.map {args =>                           //the values are the inputs to the form
+        val task = args("task").head
+        TaskListInMemoryModel.addTask(username,task)
+        Redirect(routes.TaskList1.taskList1).flashing("success"->"Task added")
+      }.getOrElse(Redirect(routes.TaskList1.taskList1).flashing("error"->"Invalid form"))
+    }.getOrElse(Redirect(routes.TaskList1.logIn).flashing("error"->"How'd you get here?")) //could use "login1", but what if someone changes the route?
+  }
+
+  def removeTask = Action{implicit request =>
+    val usernameOpt = request.session.get("username")
+    usernameOpt.map{ username =>
+      val postVals = request.body.asFormUrlEncoded    //the "argument" names in login1.scala.html are the keys in this map
+      postVals.map {args =>                           //the values are the inputs to the form
+        val index = args("index").head.toInt
+        if(TaskListInMemoryModel.removeTask(username,index)){
+          Redirect(routes.TaskList1.taskList1).flashing("success"->"Task removed")
+        } else Redirect(routes.TaskList1.taskList1).flashing("error"->"Invalid index")
+      }.getOrElse(Redirect(routes.TaskList1.taskList1).flashing("error"->"Invalid form"))
+    }.getOrElse(Redirect(routes.TaskList1.logIn).flashing("error"->"How'd you get here?")) //could use "login1", but what if someone changes the route?
   }
 }
--- server/app/models/TaskListInMemoryModel.scala
+++ server/app/models/TaskListInMemoryModel.scala
@@ -2,15 +2,15 @@ package models
 import collection.mutable
 
 object TaskListInMemoryModel{   //if the server goes down in any way, all of the changes/information is lost, so this is not ideal
-    private val users = mutable.Map[String,String]()
+    private val users = mutable.Map[String,String]("someone"->"something")
     private val tasks = mutable.Map[String,List[String]]("Jenny"->List("complete videos","task 4","task 5","stay alive"))
 
     def validateUser(username:String,password:String):Boolean = {
         users.get(username).map(_ ==password).getOrElse(false)  //check type signatures if confused
     }
 
-    def createUser(username:String,password:String):Boolean = {
-        if(users.contains(username)) false else {
+    def createUser(username:String,password:String):Boolean = { 
+        if(users.contains(username)) false else {//currently does not work (idk why)
             users(username) = password
         } 
         true
@@ -20,7 +20,15 @@ object TaskListInMemoryModel{   //if the server goes down in any way, all of the
         tasks.get(username).getOrElse(Nil)      //if the user doesnt have any tasks, return empty list
     }
 
-    def addTask(username:String,task:String):Unit = ???
+    def addTask(username:String,task:String):Unit = {
+        tasks(username) = task :: tasks.get(username).getOrElse(Nil)
+    }
 
-    def removeTask(username:String,ind:Int):Boolean = ???    //returns false if invalid index 
+    def removeTask(username:String,ind:Int):Boolean = {
+        if(ind<tasks(username).length && ind>=0 && tasks.get(username).nonEmpty){
+            tasks(username) = tasks(username).take(ind).concat(tasks(username).drop(ind+1))
+            //tasks(username)= tasks(username).patch(ind,Nil,1)     //is equivalent to above
+            true
+        } else false
+    }    //returns false if invalid index 
 }
